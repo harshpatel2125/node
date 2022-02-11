c1ced8cc403d42eb55aa16a3a5b9bc4252187fbc
harshpatel2125 <47072666+harshpatel2125@users.noreply.github.com>
2022-02-11 18:49:31 +0530

    Update getAll.js

--- services/product/getAll.js
+++ services/product/getAll.js
@@ -10,13 +10,12 @@ const productSchema = require("../../models/product");
 const Products = mongoose.model("Product", productSchema);
 
 router.get("/", async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
   try {
     const result = await Products.find().where({ deleted: false });
     if (result && result.length > 0) {
-      res.set("Access-Control-Allow-Origin", "*");
       res.status(200).send(result);
     } else {
-      res.set("Access-Control-Allow-Origin", "*");
       res
         .status(200)
         .send({ StatusCode: 404, Message: "Products Not Found..." });
