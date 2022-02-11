f8b46b696f8890feecf8d23cca6d134e9c629931
harshpatel2125 <47072666+harshpatel2125@users.noreply.github.com>
2022-02-11 18:52:01 +0530

    Add files via upload

---
+++ product/delete.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const productSchema = require("../../models/product");
+
+const router = express.Router();
+
+const Products = mongoose.model("Product", productSchema);
+
+router.delete("/:id", async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  const reqId = req.params.id;
+  try {
+    const result = await Products.findByIdAndUpdate(
+      reqId,
+      { $set: { deleted: true } },
+      { new: true }
+    );
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({
+        status: 404,
+        message: "Product from this id is not find",
+        result,
+      });
+    }
+  } catch (err) {
+    res.status(400).status({ status: 400, message: "Bad Request", err });
+  }
+});
+
+module.exports = router;
